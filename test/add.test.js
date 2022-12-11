91589281af5f66aca3ebc4b1f59bb33958b6cb6e
Shurakshya Kharel <kharel.shurakshya@gmail.com>
2022-12-11 08:13:53 +0200

    improve coverage of add.test.js and filter.test.js

--- test/add.test.js
+++ test/add.test.js
@@ -5,11 +5,15 @@ const expect = chai.expect
 
 describe("Sum", () => {
  it("adds positive number to positive number", () =>{
-     expect(sum(3,6)).to.equal(9)
+    expect(sum(3,6)).to.equal(9)
  });
 
- it("adds negative number to positive number", () =>{
-    expect(sum(-3,6)).to.equal(3)
-});
+ it("subtracts if one number is negative", () => {
+    expect(sum(-3,9)).to.equal(6)
+ });
+
+ it("Non numbers", () => {
+    expect(sum(NaN, 5)).to.deep.equal(NaN) 
+ });
 
 })
--- test/filter.test.js
+++ test/filter.test.js
@@ -4,14 +4,29 @@ const expect = chai.expect
 
 describe("filter.js Testing", () => {
 
-  const arrayToTest= [
-    { 'user': 'barney', 'active': true },
-    { 'user': 'fred',   'active': false }
-  ]
+  it("return empty array when array is null", () => {
+    const arrayToTest = null;
+    const result = [[]]
+    expect(filter(arrayToTest, ({ active }) => active)).to.deep.equal(result);
+  })
 
   it("returns user which matches the predicate", () => {
+    const arrayToTest= [
+      { 'user': 'barney', 'active': true },
+      { 'user': 'fred',   'active': false }
+    ]
     const result = [{ 'user': 'barney', 'active': true }]
     expect(filter(arrayToTest, ({ active }) => active)).to.deep.equal(result)
   })
+
+  it("returns empty array when predicate doesn't match", () => {
+    const arrayToTest= [
+      { 'user': 'barney', 'active': false },
+      { 'user': 'fred',   'active': false }
+    ]
+    const result = [[]]
+    expect(filter(arrayToTest, ({ active }) => active)).to.deep.equal(result)
+  })
+
 
 });
